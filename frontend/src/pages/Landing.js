9b15a5f183278b99f7c8b9305899e62ecfea1f92
Henoker <hennybany@gmail.com>
2023-10-04 21:41:19 +0300

    feat: landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
 
-const landing = () => {
+const Landing = () => {
   return (
-    <div>landing</div>
+    <main>
+      <nav>
+        <img src={logo} alt='jobify' className='logo'/>
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+          I'm baby authentic before they sold out cornhole blue bottle beard. Keffiyeh marxism blackbird 
+          spyplane, vexillologist etsy art party same cold-pressed. Authentic skateboard organic cardigan
+          vape neutra, yr banjo pour-over synth artisan lomo whatever hella pinterest. Austin marxism 
+          flannel cardigan YOLO, irony everyday carry farm-to-table fit pinterest deep v neutra health goth.
+          </p>
+          <button className='btn btn-hero'>
+            Login/Register
+          </button>
+        </div>
+        <img src={main} alt='job hunter' className='img main-img'/>
+      </div>
+    </main>
   )
 }
 
-export default landing
+export default Landing
