70f063530afb3731b922029cb4639b4eae1af902
Henoker <hennybany@gmail.com>
2023-10-16 20:16:44 +0300

    feat: initial test for job app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd-party apps
     "rest_framework",
+    "corsheaders",
     #Local
     "accounts.apps.AccountsConfig",
     "jobs.apps.JobsConfig",
@@ -54,12 +55,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'backend.urls'
 AUTH_USER_MODEL = "accounts.CustomUser"
--- backend/jobs/tests.py
+++ backend/jobs/tests.py
@@ -1,4 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
- 
+from .models import Job
 
-# Create your tests here.
+class JobTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+        )
+        cls.job = Job.objects.create(
+            created_by=cls.user,
+            company="ABC corporation",
+            position="Junior Software Developer",
+            status ="pending",
+            job_type="full-time",
+            job_location="Nairobi",
+        )
+def test_job_model(self):
+    self.assertEqual(self.job.created_by.username, "testuser")
+    self.assertEqual(self.job.company, "ABC corporation")
+    self.assertEqual(self.job.position, "Junior Software Developer")
+    self.assertEqual(self.job.status, "pending")
+    self.assertEqual(self.job_type, "full-time")
+    self.assertEqual(self.job_location, "Nairobi")
+   
